cb09dab933a1397350ce61d6a00d48881ec8000c
N1throuZ <nithrous@gmail.com>
2020-04-29 14:00:52 +0300

    Double parse fixed

--- BigQuery.HighLevelApi/Data/Float64DataInfo.cs
+++ BigQuery.HighLevelApi/Data/Float64DataInfo.cs
@@ -2,12 +2,13 @@
 // Unauthorized copying of this file, via any medium is strictly prohibited.
 // Proprietary and confidential.
 
+using System;
 using System.Globalization;
 using Google.Cloud.BigQuery.V2;
 
 namespace WhiteSharx.BigQuery.HighLevelApi.Data {
   public class Float64DataInfo : IDataInfo {
     public BigQueryDbType DbType => BigQueryDbType.Float64;
-    public object MapToRowValue(object source) => ((decimal) source).ToString(CultureInfo.InvariantCulture);
+    public object MapToRowValue(object source) => (Convert.ToDecimal(source)).ToString(CultureInfo.InvariantCulture);
   }
 }
